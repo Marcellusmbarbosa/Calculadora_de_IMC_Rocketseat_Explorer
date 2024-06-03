870e19e5c518671f5a3ed9188cf8a29019e7d68d
marcellusmbarbosa <marcellusmbarbosa@gmail.com>
2024-06-02 22:19:25 -0300

    Projeto Finalizado!

--- script.js
+++
@@ -1,8 +0,0 @@
-// Variáveis - Variables
-
-const form = document.querySelector('form');
-const inputWeight = document.querySelector('#weight');
-const inputHeight = document.querySelector('#height');
-
-// 3 maneiras de criar e atribuir a função a um evento
-form.onsubmit = function(){};
